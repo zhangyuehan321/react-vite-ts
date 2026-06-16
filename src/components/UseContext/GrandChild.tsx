90f6a968731a13106b20ecf8a8c6b6abcf2bc9bc
zhangyuehan321 <zhangyuehanv5@126.com>
2026-06-16 16:59:01 +0800

    consumer获取深层状态

--- src/components/UseContext/GrandChild.tsx
+++ src/components/UseContext/GrandChild.tsx
@@ -1,11 +1,18 @@
-import { useContext } from "react";
+// import { useContext } from "react";
 import { ThemeContext } from "./ThemeContext";
 
 export const GrandChild = () => {
-    const { theme } = useContext(ThemeContext);
+    // const { theme } = useContext(ThemeContext);
     return (
-        <div>
-            <div>GrandChild {theme}</div>
-        </div>
+        // <div>
+        //     <div>GrandChild {theme}</div>
+        // </div>
+        <ThemeContext.Consumer>
+            {({ theme }) => (
+                <div>
+                    <div>GrandChild {theme}</div>
+                </div>
+            )}
+        </ThemeContext.Consumer>
     )
 }
