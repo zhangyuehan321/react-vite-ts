2d75737d6ca176ab06057aadb347ad2af3845aa5
zhangyuehan321 <zhangyuehanv5@126.com>
2026-06-17 13:05:45 +0800

    泛型组件使用

--- src/components/TsHooks/List.tsx
+++ src/components/TsHooks/List.tsx
@@ -1,16 +1,26 @@
+
 //使用外部传值
-interface ListProps {
-    list: number[];
+//当传入的list类型不定时，可以使用泛型
+interface ListProps<T> {
+    list: T[];
+    onChange: (item: T) => void;
 }
-export const List = ({ list }: ListProps) => {
+export function List<T>({ list }: ListProps<T>) {
     //初始化一个数组
     // const list = [1, 2, 3, 4, 5];
 
   return(
     <div>
       {list.map(item => (
+        <>
         <div key={item}>{item}</div>
+        <button onClick={() => onChange?.(item as T)}>click</button>
+        </>
       ))}
     </div>
   )
+}
+
+export const ListDemo = () => {
+  return <List list={[1, 2, 3, 4, 5]} onChange={(item) => {console.log(item)}} />
 }
