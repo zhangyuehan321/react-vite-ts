72eb2046d37c22a7c4aebb2633b54fb157de51cf
zhangyuehan321 <zhangyuehanv5@126.com>
2026-06-17 11:50:58 +0800

    ts泛型list外部传值

--- src/components/TsHooks/List.tsx
+++ src/components/TsHooks/List.tsx
@@ -1,6 +1,10 @@
-
-export const List = () => {
-    const list = [1, 2, 3, 4, 5];
+//使用外部传值
+interface ListProps {
+    list: number[];
+}
+export const List = ({ list }: ListProps) => {
+    //初始化一个数组
+    // const list = [1, 2, 3, 4, 5];
 
   return(
     <div>
