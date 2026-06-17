d4354db44529a6bfbea23ae81bb1e9ef2da21436
zhangyuehan321 <zhangyuehanv5@126.com>
2026-06-17 14:15:13 +0800

    zod的3中校验用法

--- src/components/ZodDemo/index.tsx
+++ src/components/ZodDemo/index.tsx
@@ -1,11 +1,13 @@
 import { z } from 'zod';
 
+// 1. 定义zod schema
 const userSchema = z.object({
     name: z.string().min(1),
     age: z.number().min(18),
     email: z.string().email(),
 });
 
+// 2. 运行时数据检查
 try{
     userSchema.parse({
         name: 'John',
@@ -15,6 +17,9 @@ try{
 }catch(error){
     console.log(error);
 }
+
+// 3. 通过zod转为ts类型
+type User = z.infer<typeof userSchema>;
 
 export const ZodDemo = () => {
     return <div>ZodDemo</div>
